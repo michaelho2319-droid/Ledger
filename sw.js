79604bd5b49d1a11b0bdc05c20c5244dfce1251a
michaelho2319-droid <michael.ho2319@gmail.com>
2026-09-07 18:24:41 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Ledger service worker: caches the app shell so it opens offline. Bump CACHE when you ship a new version. */
-const CACHE = 'ledger-v2';
+const CACHE = 'ledger-v3';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png', './icons/maskable-512.png'];
 
 self.addEventListener('install', e => {
